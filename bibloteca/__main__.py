5e425d2912edd19fe5bdd6c8019b7733dbb3b063
hmekhjian <h.mekhjian@gmail.com>
2022-01-24 21:02:06 +0000

    first commit

---
+++ bibloteca/__main__.py
@@ -0,0 +1,12 @@
+"""Bibloteca entry point script."""
+# bibloteca/__main__.py
+
+from bibloteca import cli, __app_name__
+
+
+def main():
+    cli.app(prog_name=__app_name__)
+
+
+if __name__ == "__main__":
+    main()
---
+++ bibloteca/database.py
@@ -0,0 +1,8 @@
+"""This module provides the bibloteca database functionality."""
+
+# bibloteca/database.py
+
+import configparser
+from pathlib import Path
+
+from bibloteca 
