beda16f556f9fa086836246c49b444485bcfb6f6
Harouth Arthur Mekhjian <h.mekhjian@gmail.com>
2025-01-13 22:27:50 +0000

    Implement context manager for sql alchemy session

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ db_actions = dbActions()
 # db_actions.add_book('Harry Potter', 'J.K. Rowling', 500)
 # db_actions.remove_book(id= 2)
 
-# class biblotecaApp(App):
 
-print(db_actions.list_books())
+db_actions.add_book_tag(1, ["sci-fi", "prophecy"])
+
+
 
 # if __name__ == "__main__":
 #     app = biblotecaApp()
