737a9a445729aa38f47ff60b612c34d9a14fca72
Harouth Arthur Mekhjian <h.mekhjian@gmail.com>
2025-01-24 22:06:50 +0000

    api url encoding

--- book_api.py
+++ book_api.py
@@ -1,5 +1,6 @@
 import requests, json
 import configparser
+import urllib.parse
 
 
 # Parse the config file for the api key
@@ -11,13 +12,17 @@ api_key = config["google_books"]["api_key"]
 
 # Search request
 def search_book(api_key, search_term):
-    gb_api_search = 'https://www.googleapis.com/books/v1/volumes'
-    params = {
-        'q': search_term,
-        'key': api_key
-    }
+    gb_api_search = "https://www.googleapis.com/books/v1/volumes"
+    params = {"q": search_term, "key": api_key}
 
-    r = requests.get(gb_api_search, params= params)
+    r = requests.get(gb_api_search, params=params)
     data = r.json()
 
     return data
+
+
+search_term = "Harry Potter"
+safe_search_term = urllib.parse.quote(search_term)
+
+books_temp = search_book(api_key, safe_search_term)
+print(books_temp)
